316f150817540086b3048e45ed91ce085a9ff18a
Calvin Spring <SpringCalvinD@gmail.com>
2021-01-05 17:35:27 -0500

    fix: Fix manifest.in for refernce.confs

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ REQUIREMENTS = [
 
 setup(
     name="bom-configuration",
-    version="1.0.0",
+    version="1.0.1",
     description="config",
     long_description=Path("README.md").read_text(),
     author="Calvin Spring",
